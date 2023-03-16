f2315b9191f1ba8c18558cd5be29a5e4b82e20c6
Atul <atul43653@gmail.com>
2023-03-16 15:37:05 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation");
+
+// Check Employee is present or not using random
+        double randomCheck = Math.floor(Math.random()*10)%2;
+        if (randomCheck==1){
+            System.out.println("Employee is Present");
+        }else {
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
